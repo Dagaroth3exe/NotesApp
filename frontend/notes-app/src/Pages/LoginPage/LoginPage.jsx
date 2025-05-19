0b9cd48adf576e9b4757cdf8c13e5aee1b5f7263
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2025-05-19 19:01:53 +0530

    added helper and updates Login Page

--- frontend/notes-app/src/Pages/LoginPage/LoginPage.jsx
+++ frontend/notes-app/src/Pages/LoginPage/LoginPage.jsx
@@ -9,17 +9,24 @@ const LoginPage = () => {
   const [password, setPassword] = React.useState("")
   const [error, setError] = React.useState(null)
 
+  const HandleLogin = async (e) => {
+    e.preventDefault();
+  } 
+
   return <>
       <Navbar/>
 
       <div className="flex items-center justify-center mt-28 ">
         <div className="w-96 border-1 rounded px-7 py-10 bg-white border-black drop-shadow-md">
-          <form onSubmit={() => {}}>
+          <form onSubmit={HandleLogin}>
             <h4 className='text-2xl mb-7'>Login</h4>
             
-            <input type="text" placeholder="Email" className="Input-Box" />
+            <input type="text" placeholder="Email" className="Input-Box" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
 
-            <PasswordInput/>
+            <PasswordInput value={password} 
+              onChange={(e) => setPassword(e.target.value)} />
 
             <button type="submit" className="btn-primary">Login</button>
 
