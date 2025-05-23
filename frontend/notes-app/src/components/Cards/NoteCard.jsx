fd06001be8e11cc1100a347150408585e9dcedba
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2025-05-23 21:30:48 +0530

    added single noteCard

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {MdOutlinePushPin} from 'react-icons/md'
+import {MdCreate, MdDelete, MdOutlinePushPin} from 'react-icons/md'
 
 const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, onPinNote}) => {
   return (
@@ -12,7 +12,22 @@ const NoteCard = ({title, date, content, tags, isPinned, onEdit, onDelete, onPin
 
             <MdOutlinePushPin className="" onClick={onPinNote}/>
         </div>
+        
         <p className="">{content?.slice(0, 60)}</p>
+
+        <div className="">
+            <div className="text-xs text-slate-500">{tags}</div>
+            <div className="flex items-center gap-2">
+                <MdCreate
+                className="icon-btn hover:text-green-600"
+                onClick={onEdit}
+                />
+                <MdDelete 
+                className="icon-btn hover:text-red-600"
+                onClick={onDelete}
+                />
+            </div>
+        </div>
     </div>
   )
 }
